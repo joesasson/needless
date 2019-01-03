a88c96ee54f9ecd03d187c23c5b3476de0cabc39
Joe Sasson <ysasson88@gmail.com>
2019-01-03 10:42:53 -0500

    Add nordstrom invoice

--- src/nordstromInv.ts
+++ src/nordstromInv.ts
@@ -4,10 +4,10 @@ function nordstromInv() {
   const { ss, sheetData } = getSheetData()
   const wrapped = new SheetData(sheetData)
   const { retailer_create_date, po_number, ship_first_name, ship_last_name, ship_address_1, ship_address_2
-  ship_city, ship_region, ship_postal, ship_method, line_item_sku, line_item_expected_cost
-  line_item_quantity, in_stock } = wrapped.reduceHeaders()
+  ship_city, ship_region, ship_postal, ship_country, ship_method, line_item_sku, line_item_expected_cost
+  line_item_quantity, in_stock, ship_phone } = wrapped.reduceHeaders()
 
-  let newData = wrapped.data.map((row, i) => {
+  let invData = wrapped.data.map((row, i) => {
     // transforms
       // name
     let name = i > 0 ? `${row[ship_first_name]} ${row[ship_last_name]}`.replace("  ", " ") : "name"
@@ -26,7 +26,66 @@ function nordstromInv() {
       row[line_item_quantity], 
     ]
   })
+  Logger.log(mapQtys(wrapped.data))
+
+  let shipData = wrapped.content.map(row => {
+    // constants
+    let packagingType = "2" // "other" per ups - https://www.ups.com/us/en/shipping/create/shipping/create/batch-file.page # packaging type codes
+    // extracts
+    // transforms
+      // name - combine first and last names
+      // weight - summarize multilines
+      // method - convert to code
+    // filters
+      // multilines - if a po is the same as previous line, remove it
+    let name = `${row[ship_first_name]} ${row[ship_last_name]}`.replace("  ", " ")
+    let weight = calculateWeight(row[line_item_quantity])
+    let shipMethod = row[ship_method] === "2nd Day Air" ? "2" : "3" // if method is not 2nd day air, assume it's ground
+    return [
+      " ", // empty space for company
+      name,
+      row[ship_country],
+      row[ship_address_1],
+      row[ship_address_2],
+      "",
+      row[ship_city],
+      row[ship_region],
+      row[ship_phone],
+      ...addSpaces(3),
+      packagingType,
+      "",
+      weight,
+      ...addSpaces(8),
+      shipMethod
+    ]
+  })
 
   // create a new sheet
-  createNewSheetWithData(ss, newData, "Invoice Import")
+  createNewSheetWithData(ss, invData, "Invoice Import")
+  createNewSheetWithData(ss, shipData, "Batch File for UPS.com")
+}
+
+const addSpaces = numSpaces => {
+  let a = []
+  for(let i = 0; i < numSpaces; i++){
+    a.push("")
+  }
+  return a
+}
+
+const calculateWeight = qty => Math.ceil(qty * 1.2 + 1)
+
+const mapQtys = sheetData => {
+  let { po_number, line_item_quantity } = reduceHeaders(sheetData)
+  let qtyMap = {}
+  return sheetData.forEach( row => {
+    let po = row[po_number]
+    let qty = row[line_item_quantity]
+    Logger.log(qty)
+    if(qtyMap[po]){
+      return qtyMap[po].qty += qty
+    } else {
+      return qtyMap[po].qty = qty
+    }
+  })
 }
